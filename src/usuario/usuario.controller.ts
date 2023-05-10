eb1db7a7fb56291269cd29a8ce0607a7fdbeac73
fabio <fabiuhp@msn.com>
2023-05-09 22:40:31 -0300

    dependency inversion

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -3,11 +3,12 @@ import { UsuarioRepository } from './usuario.repository';
 
 @Controller('/usuarios')
 export class UsuarioController {
-  private usuarioRepository = new UsuarioRepository();
+  constructor(private usuarioRepository: UsuarioRepository) {}
 
   @Post()
   async criaUsuario(@Body() dadosDoUsuario) {
-    return this.usuarioRepository.salvar(dadosDoUsuario);
+    this.usuarioRepository.salvar(dadosDoUsuario);
+    return dadosDoUsuario;
   }
 
   @Get()
