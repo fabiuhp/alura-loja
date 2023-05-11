486b34391d9256fb5876ae287d221f271f6a2da0
fabio <fabiuhp@msn.com>
2023-05-11 17:04:25 -0300

    uma serie de coisas

---
+++ src/produto/produto.entity.ts
@@ -0,0 +1,54 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+// class CaracteristicaProduto {
+//   nome: string;
+//   descricao: string;
+// }
+
+// class ImagemProduto {
+//   url: string;
+//   descricao: string;
+// }
+
+@Entity({ name: 'produtos' })
+export class ProdutoEntity {
+  @PrimaryGeneratedColumn('uuid', { name: 'id' })
+  id: string;
+
+  @Column({ name: 'usuario_id', length: 100, nullable: false })
+  usuarioId: string;
+
+  @Column({ name: 'nome', length: 100, nullable: false })
+  nome: string;
+
+  @Column({ name: 'valor', nullable: false })
+  valor: number;
+
+  @Column({ name: 'quantidade', nullable: false })
+  quantidade: number;
+
+  @Column({ name: 'quantidade', length: 255, nullable: false })
+  descricao: string;
+
+  @Column({ name: 'categoria', length: 100, nullable: false })
+  categoria: string;
+
+  // caracteristicas: CaracteristicaProduto[];
+  // imagens: ImagemProduto[];
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: string;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: string;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: string;
+}
